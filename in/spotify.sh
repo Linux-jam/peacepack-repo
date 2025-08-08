df64a0742412ec74abe182daf04ddf06b7970ce2
Fuma Nakamura <95830789+kons10@users.noreply.github.com>
2025-08-08 20:50:04 +0900

    wget

--- in/spotify.sh
+++ in/spotify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+wget -qO- https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb https://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
 apt update
 apt install spotify-client -y
